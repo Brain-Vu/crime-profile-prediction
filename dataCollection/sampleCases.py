881102166b62906ae7f01eb4f1688dee6d04e774
Brian Vu <brian.vu18@gmail.com>
2026-08-12 11:54:30 -0700

    slight edit to sampling code

--- dataCollection/sampleCases.py
+++ dataCollection/sampleCases.py
@@ -1,5 +1,13 @@
-import random
+import time
+import requests
 import pandas as pd
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+API_TOKEN = os.getenv("COURT_LISTENER_API_KEY")
+BASE_URL = "https://www.courtlistener.com/api/rest/v4/opinions"
 
 # ----------------------------
 # CONFIGURATION
@@ -9,19 +17,45 @@ RANDOM_SEED = 67
 SAMPLE_SIZE = 165
 
 file = input("Please input the name of the csv: ")
-casesDF = pd.read_csv(f"./data/{file}")
+cases_DF = pd.read_csv(f"./data/{file}")
+
+headers = {
+    "Authorization": f"Token {API_TOKEN}"
+}
 
 # ----------------------------
 # RANDOM SAMPLE
 # ----------------------------
 
-sampleDF = casesDF.sample(n=SAMPLE_SIZE, random_state=RANDOM_SEED)
-print(sampleDF)
+sample_DF = cases_DF.sample(n=SAMPLE_SIZE, random_state=RANDOM_SEED)
+print(sample_DF)
+
+# ----------------------------
+# GETTING PLAIN TEXT DESCRIPTIONS
+# ----------------------------
+
+for case in sample_DF.itertuples(index=True):
+  id = case.case_id
+  index = case.Index
+
+  response = requests.get(
+    f"{BASE_URL}/{id}",
+    headers=headers
+  )
+
+  # error handling
+  if response.status_code != 200:
+      print("Error:", response.status_code)
+      break
+
+  opinion_data = response.json()
+  sample_DF.loc[index, "plain_text"] = opinion_data["plain_text"]
+
+  time.sleep(3.4)
 
 # ----------------------------
 # SAVE FILES
 # ----------------------------
 
-# df.to_csv("courtlistener_sample.csv", index=False)
-
-# print(f"\nSaved {sample_size} randomly selected cases.")
+sample_DF.to_csv("samples.csv", index=False)
+print(f"\nSaved {SAMPLE_SIZE} randomly selected cases.")
